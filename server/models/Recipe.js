f794618f57f93cbe4aee51427262b17a2b4f528a
urfavteddybear <business.urfavteddybear@gmail.com>
2023-01-26 09:17:33 +0800

    Adding Australian category

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -19,7 +19,7 @@ const recipeSchema = new mongoose.Schema({
   },
   category: {
     type: String,
-    enum: ['Thai', 'American', 'Chinese', 'Mexican', 'Indian', 'Indonesian', 'Malaysian'],
+    enum: ['Thai', 'American', 'Chinese', 'Mexican', 'Indian', 'Indonesian', 'Malaysian', 'Australian'],
     required: 'This field is required.'
   },
   image: {
